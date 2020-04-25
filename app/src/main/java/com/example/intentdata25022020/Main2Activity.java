34d272f0cf27f26f67e06f6efad804deb7543572
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-25 21:33:24 +0700

    send String

--- app/src/main/java/com/example/intentdata25022020/Main2Activity.java
+++ app/src/main/java/com/example/intentdata25022020/Main2Activity.java
@@ -2,7 +2,9 @@ package com.example.intentdata25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        if (intent != null){
+            String value = intent.getStringExtra("chui");
+            Log.d("BBB",value);
+        }
     }
 }
--- app/src/main/java/com/example/intentdata25022020/MainActivity.java
+++ app/src/main/java/com/example/intentdata25022020/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.intentdata25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnString = findViewById(R.id.buttonString);
+
+        mBtnString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("chuoi","Xin chao main 2");
+                startActivity(intent);
+            }
+        });
     }
 }
