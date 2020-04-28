09885c8523d5a0d9c7c5f7802f359609396571d5
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-28 20:22:15 +0700

    arraylist

--- app/src/main/java/com/example/intentdata25022020/MainActivity.java
+++ app/src/main/java/com/example/intentdata25022020/MainActivity.java
@@ -11,10 +11,11 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mBtnString,mBtnArray;
+    Button mBtnString,mBtnArray,mBtnArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         mBtnString = findViewById(R.id.buttonString);
         mBtnArray = findViewById(R.id.buttonArray);
+        mBtnArrayList = findViewById(R.id.buttonArrayList);
 
         mBtnString.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +40,18 @@ public class MainActivity extends AppCompatActivity {
                 String[] strings = {"A","B","C","D","E","F"};
                 Intent intent = new Intent(MainActivity.this,Main2Activity.class);
                 intent.putExtra("array",strings);
+                startActivity(intent);
+            }
+        });
+
+        mBtnArrayList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> arrayList = new ArrayList<>(
+                        Arrays.asList("A","B","C","D")
+                );
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("arraylist",arrayList);
                 startActivity(intent);
             }
         });
