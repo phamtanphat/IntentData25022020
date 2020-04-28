521ab49583ca6acca0b0691e95f915ef0a23d91b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-28 20:14:32 +0700

    type array

--- app/src/main/java/com/example/intentdata25022020/MainActivity.java
+++ app/src/main/java/com/example/intentdata25022020/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.intentdata25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Person;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mBtnString;
+    Button mBtnString,mBtnArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mBtnString = findViewById(R.id.buttonString);
+        mBtnArray = findViewById(R.id.buttonArray);
 
         mBtnString.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +31,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mBtnArray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] strings = {"A","B","C","D","E","F"};
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("array",strings);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
